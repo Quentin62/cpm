bce847054e68c45051bccaadb6f36a94ccb222c1
Quentin Grimonprez <qgrimonprez@diagrams-technologies.com>
2020-10-30 09:07:08 +0100

    add na.rm = TRUE

--- R/detectChangePointBatch.R
+++ R/detectChangePointBatch.R
@@ -1,32 +1,35 @@
-detectChangePointBatch <- function(x,cpmType, alpha=0.05, lambda=NA) {
-    if (length(x) < 5) {
-        print("Error: Batch change detectoin requires a sequence of length 5 or greater")
-        return()
-    }
-    args <- verifyArguments(cpmType,500,20,lambda)
-    if (args$success==FALSE) {
-        return()
-    }
-    
-    cpmType <- args$cpmType
-       
-    if(is.na(lambda)){lambda<-0}    
-    
-    threshold <- NA
-    if (!is.na(alpha)) {
-    	threshold <- getBatchThreshold(cpmType,alpha,length(x),lambda)
-    }
-  
-    res <-.C("cpmDetectChangeBatch",as.character(cpmType),as.double(x),as.integer(length(x)),Ds=as.double(numeric(length(x))),lambda=as.double(lambda),PACKAGE="cpm")
+detectChangePointBatch <- function(x, cpmType, alpha = 0.05, lambda = NA) {
+  if (length(x) < 5) {
+    print("Error: Batch change detectoin requires a sequence of length 5 or greater")
+    return()
+  }
+  args <- verifyArguments(cpmType, 500, 20, lambda)
+  if (args$success == FALSE) {
+    return()
+  }
 
-	changeDetected <- NA; 
-    changePoint <- which.max(res[["Ds"]])
-	if (!is.na(alpha)) {
-    	changeDetected <- max(res[["Ds"]]) > threshold
-        if (changeDetected==FALSE) {changePoint<-0}
+  cpmType <- args$cpmType
+
+  if (is.na(lambda)) {
+    lambda <- 0
+  }
+
+  threshold <- NA
+  if (!is.na(alpha)) {
+    threshold <- getBatchThreshold(cpmType, alpha, length(x), lambda)
+  }
+
+  res <- .C("cpmDetectChangeBatch", as.character(cpmType), as.double(x), as.integer(length(x)), 
+            Ds = as.double(numeric(length(x))), lambda = as.double(lambda), PACKAGE = "cpm")
+
+  changeDetected <- NA
+  changePoint <- which.max(res[["Ds"]])
+  if (!is.na(alpha)) {
+    changeDetected <- max(res[["Ds"]], na.rm = TRUE) > threshold
+    if (!changeDetected) {
+      changePoint <- 0
     }
-    return(list(x=x,Ds=res[["Ds"]],changePoint=changePoint, changeDetected=changeDetected, alpha=alpha,threshold=threshold))
+  }
+  return(list(x = x, Ds = res[["Ds"]], changePoint = changePoint, changeDetected = changeDetected, 
+              alpha = alpha, threshold = threshold))
 }
-
-
-
